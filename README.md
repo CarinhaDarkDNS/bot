2f6e06ab863f917b7d7930bc7949e8ae2cdc2c28
CarinhaDarkDNS <carinha.dark@gmail.com>
2020-12-29 19:06:43 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,11 @@
+## <img src="https://github.com/TheDudeThatCode/TheDudeThatCode/blob/master/Assets/Hi.gif" width="29px"> TRADUZIDO POR: CarinhaDarkDNS 
+<p align="center">
+<img src="https://images.app.goo.gl/njrMbxBXV7zRm9kR7" width="230" height="230"/>
+</p>
+<br>
+
+
+ 
+</details>
+
+### teste
---
+++ src/help.js
@@ -0,0 +1,27 @@
+const help = (prefix) => {
+	return `🔘 *BlazeHosting - BOT* 🔘\n\n
+🔻 *Comandos Admins* 🔻
+▪ command: *${prefix}marcar*
+desc : marque todos membros do grupo. *(somente admins)*\n\n
+▪ command:  *${prefix}whatsapp*
+desc: pegar o link do grupo. *(somente admins)*\n\n
+▪ command:  *${prefix}discord*
+desc: pegar o link do grupo. *(somente admins)*\n\n
+▪ command:  *${prefix}clear*
+desc: limpar chat (0-25). *(somente admins)*\n\n
+▪ command:  *${prefix}kick*
+desc: remover um membro do grupo. *(somente admins)*\n\n
+▪ command:  *${prefix}add*
+desc: adicionar um membro no grupo. *(somente admins)*\n\n\n
+🔻 *Comandos Liberados* 🔻
+▪ command:  *${prefix}info*
+desc: ver a informação do bot.\n\n
+▪ command:  *${prefix}admins*
+desc: ver a lista de admins do grupo.\n\n
+▪ command:  *${prefix}sticker*
+desc: marque uma foto / gif / video.\n\n
+}
+
+exports.help = comandos
+
+
